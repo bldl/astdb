fc03d17f1538b29184217d783aae128ba9653648
Patrick Monslaup <patrick.bergen@hotmail.com>
2015-06-09 18:49:25 +0200

    Added Javadoc to most methods. Note that MongoAST is broken now until code is completely refactored/restructured.

---
+++ src/org/magnolialang/magnolia/repr/Entry.java
@@ -0,0 +1,76 @@
+package org.magnolialang.magnolia.repr;
+
+/**
+ * Every entry is on the form
+ * 
+ * entry : {
+ * .. identity : <Identity>, //the identity of the node to which it belongs
+ * .. key : <Key<?>>,
+ * .. data : <?>
+ **/
+
+public class Entry<T> {
+
+	private Key<T> KEY;
+	private T VALUE;
+	private Identity NODEID;
+
+
+	public Entry(Key<T> key, T value) {
+		if(key == null) {
+			throw new IllegalArgumentException("key must be non-null");
+		}
+		if(value == null) {
+			throw new IllegalArgumentException("value must be non-null");
+		}
+
+		KEY = key;
+		VALUE = value;
+	}
+
+
+	/**
+	 * Gets the key of an Entry
+	 * 
+	 * @return Key<T> the key of this entry
+	 */
+	public Key<T> getKey() {
+		return KEY;
+	}
+
+
+	/**
+	 * Gets the node to which this entry belongs
+	 * 
+	 * @return Identity the Identity of the node which this belongs to, or null
+	 *         if the entry doesn't yet belong to a Node
+	 */
+	public Identity getNodeId() {
+		return NODEID;
+	}
+
+
+	/**
+	 * Gets the value stored in this entry
+	 * 
+	 * @return T the value stored in this entry
+	 */
+	public T getValue() {
+		return VALUE;
+	}
+
+
+	/**
+	 * Sets the NODEID of this Entry to be "IDENTITY". Throws a runtimeException
+	 * if it already belongs to a Node!
+	 * 
+	 * @param IDENTITY
+	 *            the Identity of the Node to which this Entry is associated.
+	 */
+	public void setNodeId(Identity IDENTITY) {
+		if(NODEID != null) {
+			throw new RuntimeException("Entry already belongs to another node!");
+		}
+		NODEID = IDENTITY;
+	}
+}
---
+++ src/org/magnolialang/magnolia/repr/Node.java
@@ -0,0 +1,91 @@
+package org.magnolialang.magnolia.repr;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.magnolialang.magnolia.repr.impl.AnonIdentity;
+import org.magnolialang.magnolia.repr.impl.NamedIdentity;
+
+/**
+ * Every node in the graph is on the form
+ * 
+ * node : {
+ * .. identity : <Identity>,
+ * .. name : <String>, // name of the node
+ * .. parent : <Identity>, //identity of its parent
+ * .. entries : <Entry>, //the entries of the Node
+ * .. children : <List<Node>> // the child-nodes of the Node
+ * }
+ **/
+public class Node {
+	final Identity IDENTITY;
+	final String name;
+	Identity parent;
+	List<Node> children;
+	List<Entry<?>> entries;
+
+
+	public Node(String name, Identity parent, Node... childNodes) {
+		IDENTITY = makeId(name);
+		this.name = name;
+		this.parent = parent;
+
+		if(childNodes != null) {
+			children = new ArrayList<Node>();
+		}
+
+		for(Node child : childNodes) {
+			child.parent = IDENTITY;
+			children.add(child);
+		}
+	}
+
+
+	public Node(String name, Node... childNodes) {
+		IDENTITY = makeId(name);
+		this.name = name;
+		parent = null;
+
+		if(childNodes != null) {
+			children = new ArrayList<Node>();
+		}
+
+		for(Node child : childNodes) {
+			child.parent = IDENTITY;
+			children.add(child);
+		}
+	}
+
+
+	public <V> Node addEntry(Entry<V> entry) {
+		entry.setNodeId(IDENTITY);
+		entries.add(entry); //TODO check duplicates maybe?
+		return this;
+	}
+
+
+	public Identity getIDENTITY() {
+		return IDENTITY;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public Identity getParent() {
+		return parent;
+	}
+
+
+	private Identity makeId(String name) {
+		return (name == null || name == "") ? new AnonIdentity() : new NamedIdentity(name); //might want to make id's in another way
+	}
+
+//
+//	public void setParent(Identity parent) {
+//		// TODO update children list of previous parent and we also need to be sure that this doesn't allow for mess-ups in database
+//		this.parent = parent;
+//	}
+}
